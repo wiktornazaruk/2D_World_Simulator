a75647f1d417b030e1a55336f4444ff61013ce68
Wiktor Nazaruk <89415407+wiktornazaruk@users.noreply.github.com>
2023-08-21 00:53:42 +0200

    Update music.py

--- game/music.py
+++ game/music.py
@@ -1,5 +1,5 @@
 music = {
-    "lullaby" : "C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/music/Volume Alpha 03. Subwoofer Lullaby.mp3",
-    "death": "C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/music/Volume Alpha 04. Death.mp3",
-    "og": "C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/music/Volume Alpha 08. Minecraft.mp3"
+    "lullaby" : "music/Volume Alpha 03. Subwoofer Lullaby.mp3",
+    "death": "music/Volume Alpha 04. Death.mp3",
+    "og": "music/Volume Alpha 08. Minecraft.mp3"
 }
