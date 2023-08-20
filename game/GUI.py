1cf33a6de8949fee70b80842acb6bc4ea600bf33
Wiktor Nazaruk <89415407+wiktornazaruk@users.noreply.github.com>
2023-08-21 00:55:00 +0200

    Update GUI.py

--- game/GUI.py
+++ game/GUI.py
@@ -12,19 +12,19 @@ class GUI:
         self.root.resizable(width=False, height=False)
         # loading icons
         self.icon = [
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Empty.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Human.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Sheep.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Wolf.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Antelope.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Turtle.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Fox.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Cybersheep.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Grass.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/SowThistle.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Guarana.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Belladonna.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
-            ImageTk.PhotoImage(Image.open('C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Hogweed.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS))
+            ImageTk.PhotoImage(Image.open('icons/Empty.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Human.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Sheep.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Wolf.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Antelope.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Turtle.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Fox.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Cybersheep.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Grass.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/SowThistle.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Guarana.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Belladonna.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS)),
+            ImageTk.PhotoImage(Image.open('icons/Hogweed.png').resize((var["tile_size"], var["tile_size"]), Image.ANTIALIAS))
         ]
         # placing cells
         self.box = [
@@ -46,7 +46,7 @@ class GUI:
         self.root.bind_all('<Key>', self.key)
         self.world = World()
         self.root.title('2D World Simulator Wiktor Nazaruk s190454')
-        self.root.iconphoto(False, ImageTk.PhotoImage(file='C:/Users/wikto/Desktop/Studia/sem2/oop/2D_World_Simulator/python/icons/Earth.png'))
+        self.root.iconphoto(False, ImageTk.PhotoImage(file='icons/Earth.png'))
         self.world.mainLoop(self)
 
     # display function
